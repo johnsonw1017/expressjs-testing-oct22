8a228b168841a27aea752b5ae3349abe4b532cf0
Johnson Wang <106476588+johnsonw1017@users.noreply.github.com>
2023-06-08 19:18:04 +1000

    feat!: moved signup logic to middleware

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,17 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/signup", (request, response) => {
+const {bodyHasEmail, bodyHasPassword, emailIsValid} = require("../middleware/UserMiddleware");
 
-    if (request.body.email 
-        &&
-        request.body.email.includes("@") 
-        &&
-        request.body.password){
+router.post("/signup", 
+    //check for email
+    bodyHasEmail,
+    //check if email is valid
+    emailIsValid,
+    //check for password
+    bodyHasPassword,
+    
+    (request, response) => {
+        
+        // assume if we reached this point
+        // middleware did not throw errors
+
         response.json({message:"Sign up success!"});
-    } else {
-        response.status(400).json({message:"Sign up failure!"});
-    }
+
+    // if (request.body.email 
+    //     &&
+    //     request.body.email.includes("@") 
+    //     &&
+    //     request.body.password){
+    //     response.json({message:"Sign up success!"});
+    // } else {
+    //     response.status(400).json({message:"Sign up failure!"});
+    // }
 });
 
 module.exports = router;
---
+++ src/middleware/UserMiddleware.js
@@ -0,0 +1,30 @@
+
+
+const bodyHasEmail = (request, response, next) => {
+    if(request.body.email){
+        next();
+    } else {
+        //add error to error stack
+    }
+
+}
+const bodyHasPassword = (request, response, next) => {
+    if(request.body.password){
+        next();
+    } else {
+        //add error to error stack
+    }
+}
+const emailIsValid = (request, response, next) => {
+    if(request.body.email.includes("@")){
+        next();
+    } else {
+        //add error to error stack
+    }
+}
+
+module.exports = {
+    bodyHasEmail,
+    bodyHasPassword,
+    emailIsValid
+}
